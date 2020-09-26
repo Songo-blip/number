07f47b12b5f3e8a273d92349822bffd8374e4338
Songo-blip <70211574+Songo-blip@users.noreply.github.com>
2020-09-26 21:35:54 +0200

    Add files via upload

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumbersToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+                {
+                    string[] words = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+                    string[] manywords = { "eleven", "twelve", "thirteen", "fourteen", "firfteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                    string[] manywords2 = { "twenty", "thirthy", "forthy", "fifty", "sixty", "seventy", "eighty", "ninty" };
+                    {
+                        for (int counter = 0; counter < 70; counter++)
+                            // int Coverter = counter-- > words;
+                            foreach (string word in manywords2)
+                            {
+                                                                
+                                {
+                                        string s = Convert.ToString(counter.ToString);
+                                
+
+                        //  foreach (string word in manywords2)
+                            {
+                            Console.WriteLine("");
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+}
+
